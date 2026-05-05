f9b3f39c21befd520d588eec46f24a2d1b88072d
zsoelekt <114098128+zsoelekt@users.noreply.github.com>
2026-05-04 20:46:45 -0500

    Update servicio.py

--- modelos/servicio.py
+++ modelos/servicio.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+class Servicio(ABC):
+    def __init__(self, nombre, tarifa):
+        self.nombre = nombre
+        self.tarifa = tarifa
+
+    @abstractmethod
+    def calcular_costo(self, horas):
+        pass
+
+    @abstractmethod
+    def describir(self):
+        pass
