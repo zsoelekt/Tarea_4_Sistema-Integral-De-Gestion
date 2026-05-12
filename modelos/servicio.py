e8ecec32ee257a31f96c0b840ab7b64e940b781c
zsoelekt <114098128+zsoelekt@users.noreply.github.com>
2026-05-12 17:08:45 -0500

    Update servicio.py
    Correción Codigo

--- modelos/servicio.py
+++ modelos/servicio.py
@@ -1,14 +1,11 @@
 from abc import ABC, abstractmethod
 
 class Servicio(ABC):
-    def __init__(self, nombre, tarifa):
+
+    def __init__(self, nombre, precio_base):
         self.nombre = nombre
-        self.tarifa = tarifa
+        self.precio_base = precio_base
 
     @abstractmethod
     def calcular_costo(self, horas):
-        pass
-
-    @abstractmethod
-    def describir(self):
         pass
